f7f2a47e9da0ce909138855594f7fe0f6829c957
Tyler Collier <tyler.collier+github@devmounta.in>
2018-08-15 12:12:57 -0600

    Fix bugs

--- server/middlewares/devmtn-session.js
+++ server/middlewares/devmtn-session.js
@@ -1,24 +1,20 @@
+const sessions = {};
+let nextSessionId = 1;
 module.exports = function(req, res, next) {
-  const sessions = {};
-  let nextSessionId = 1;
-  
   function createSession() {
     const newSession = {};
     req.session = newSession;
     sessions[nextSessionId] = newSession;
-    res.setHeader('set-cookie', 'sessionId=' + nextSessionId);
+    res.setHeader('set-cookie', 'sessionId=' + nextSessionId + '; path=/;');
     nextSessionId++;
   }
   
   if (req.headers.cookie) {
-    // sessionId=1
-    // [sessionId, 1]
     const sessionId = req.headers.cookie.split('=')[1];
-    console.log('-------------- sessionId', sessionId);
     // Look up session by cookie/session-id
     if (sessions[sessionId]) {
       req.session = sessions[sessionId];
-      res.setHeader('set-cookie', 'sessionId=' + sessionId);
+      res.setHeader('set-cookie', 'sessionId=' + sessionId + '; path=/;');
     } else {
       createSession();
     }
